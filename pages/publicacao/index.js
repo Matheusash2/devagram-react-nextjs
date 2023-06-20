ab4369bfc003b68e2205e7e65a32bcf3f8a64d46
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-06-19 23:36:28 -0300

    Finalização da publicação

--- pages/publicacao/index.js
+++ pages/publicacao/index.js
@@ -5,11 +5,20 @@ import { useState } from "react";
 import Botao from "@/componentes/botao";
 import imagemPublicacaoPadrao from "../../public/imagens/imagemPublicacao.svg";
 import imagemSetaEsquerda from "../../public/imagens/setaEsquerda.svg";
+import FeedService from "@/services/FeedService";
+import { useRouter } from "next/router";
+
+
+const limiteDaDescricao = 255;
+const descricaoMinima = 3;
+const feedService = new FeedService();
 
 function Puclicacao() {
     const [imagem, setImagem] = useState();
     const [inputImagem, setInputImagem] = useState();
     const [etapaAtual, setEtapaAtual] = useState(1);
+    const [descricao, setDescricao] = useState('');
+    const router = new useRouter();
 
     const estaNaEtapaUm = () => etapaAtual === 1;
 
@@ -42,9 +51,55 @@ function Puclicacao() {
     }
 
     const aoClicarAcaoDireitaCabecalho = () => {
-        setEtapaAtual(2);
+        if (estaNaEtapaUm()) {
+            setEtapaAtual(2);
+            return;
+        }
+        publicar()
     }
 
+    const escreverDescricao = (e) => {
+        const valorAtual = e.target.value;
+        if (valorAtual.length >= limiteDaDescricao) {
+            return;
+        }
+
+        setDescricao(valorAtual);
+    }
+
+    const obterClassNameCabecalho = () => {
+        if (estaNaEtapaUm()) {
+            return 'primeiraEtapa';
+        }
+        return 'segundaEtapa';
+    }
+
+    const publicar = async () => {
+        try {
+            if (!validarFormulario()) {
+                alert('A descrição precisa de pelo menos 3 caracteres e a imagem precisa estar selecionada!');
+                return;
+            }
+
+            const corpoPublicacao = new FormData();
+            corpoPublicacao.append('file', imagem.arquivo);
+            corpoPublicacao.append('descricao', descricao);
+            
+            await feedService.fazerPublicacao(corpoPublicacao);
+            router.push('/');
+
+        } catch (error) {
+            alert('Erro ao salvar publicação!');
+        }
+    }
+
+    const validarFormulario = () => {
+        return (
+            descricao.length >= descricaoMinima
+            && imagem?.arquivo    
+        );
+    }
+    
     return (
         <div className="paginaPublicacao largura30pctDesktop">
             <CabecalhoComAcoes
@@ -53,6 +108,7 @@ function Puclicacao() {
                         ? null 
                         : imagemSetaEsquerda
                 }
+                className={obterClassNameCabecalho()}
                 textoEsquerda={obterTextoEsquerdaCabecalho()}
                 aoClicarAcaoEsquerda={aoClicarAcaoEsquerdaCabecalho}
                 elementoDireita={obterTextoDireitaCabecalho()}
@@ -85,9 +141,22 @@ function Puclicacao() {
                     </div>
                     )
                     : (
-                        <div className="segundaEtapa">
-                            
-                        </div>
+                        <>
+                            <div className="segundaEtapa">
+                                <UploadImagem
+                                    setImagem={setImagem}
+                                    imagemPreview={imagem?.preview} />
+
+                                <textarea
+                                    rows={3}
+                                    value={descricao}
+                                    placeholder="Escreva uma legenda..."
+                                    onChange={escreverDescricao}
+                                ></textarea>
+                            </div>
+
+                            <hr className="linhaDivisoria" />
+                        </>
                     )
                 }
             </div>
